c2a8f1b5c25be800f3053887c4951e1143ee10a3
shahdev kumar <shahdevkumar.d@gmail.com>
2025-12-07 22:51:08 +0500

    javascript

---
+++ index.js
@@ -0,0 +1,91 @@
+// // // this is the code for sum 
+// // int age=23
+// // age+2 ;
+// // out put will be 23 
+// // ouput will be only 25 if age =age+2;
+
+
+// let a=5;
+
+//NaN
+
+// let em = " ";
+// // this is a valid 
+
+// emp = " ";
+// // this is also valid
+
+// let role = 'irom';
+// let num = '23';
+// let empty= " ";
+
+// let str = "abcdefghif   gfgfgfg";
+
+
+// // if we want to print  like 'i have "given" this'
+// let senetexce  = ' this is "apple"';
+// // this is valid
+
+// let sentece = "this is 'ine' ";
+// // this is valid
+
+// let sente = " this is "one"";
+// // this is error
+
+// let sens = 'i am 'good'd';
+// // this is not valid gives error
+
+
+
+// let name = "TONNY STARK";
+// // java base is zero base indexing.
+// name[0]= 'T';
+// name.length 
+// typeof name.length;
+// name [name.length];
+// name[name.length-1] ;
+// name[name.length-2] ;
+// name[name.length-3] ;
+
+
+
+// "tony" +" "+"Stack ";
+// "tony"+ 1;
+
+// undefined and null
+
+// let name;
+// // undefined
+// type of name // undefined
+
+
+
+// let a = null;
+// // value of a is null mans absence of value.
+
+// let year =null;
+// year = 2000;
+
+
+// let name = "Shahdev"
+// name.length;
+
+// let firstname = "Shahdev";
+// firstname[0];
+// firstname.length-1;
+//  firstname[6];
+//  firstname[firstname.length-1];
+
+
+
+// "shadev"+123
+
+
+
+// let empty = "";
+// empty.length which is 0
+
+// let space = " ";
+// space.length which is 1
+
+
